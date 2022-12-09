d19eeba932a2764406b6ec84f935e8db0773418d
Hung <hoangvanhung6876@gmail.com>
2022-12-09 17:35:41 +0700

    get showtime by cinema

--- controllers/showtimeController.js
+++ controllers/showtimeController.js
@@ -3,6 +3,52 @@ const Movie = require("../models/Movie");
 const Room = require("../models/Room");
 const asyncHandler = require("express-async-handler");
 const Province = require("../models/Province");
+const Cinema = require("../models/Cinema");
+
+const getShowtimeByCinema = asyncHandler(async (req, res) => {
+  const { date, cinemaId } = req.params;
+
+  if (!date && !cinemaId)
+    return res.status(400).json({ message: "cinemaId, date is required!" });
+  let currentDate = new Date(date);
+  let nextDate = new Date(date);
+  const showtimes = [];
+  const cinema = await Cinema.findById(cinemaId);
+  const movies = await Movie.find();
+
+  for (const movie of movies) {
+    const filter = {
+      $and: [
+        {
+          date: {
+            $gte: date,
+            $lt: nextDate.setDate(currentDate.getDate() + 1),
+          },
+        },
+
+        { roomId: { $in: cinema.rooms } },
+        { movieId: { $eq: movie._id } },
+      ],
+    };
+
+    const showtimesMovies = await Showtime.find(filter)
+      .populate("movieId")
+      .populate("roomId");
+
+    showtimesMovies.sort(function (a, b) {
+      const aHour = a.time?.split(":")[0];
+      const bHour = b.time?.split(":")[0];
+      if (aHour == bHour) return a.time?.split(":")[1] - b.time?.split(":")[1];
+      return aHour - bHour;
+    });
+
+    showtimes.push({
+      movie: movie,
+      showtimes: [...showtimesMovies],
+    });
+  }
+  res.json(showtimes);
+});
 
 const getShowtimesByProvince = asyncHandler(async (req, res) => {
   const { provinceId, date, movieId } = req.params;
@@ -30,7 +76,7 @@ const getShowtimesByProvince = asyncHandler(async (req, res) => {
       ],
     };
     if (movieId !== "null") {
-      filter.$and.push({ movieId: { $eq: movieId } },)
+      filter.$and.push({ movieId: { $eq: movieId } });
     }
     const showtimesCinema = await Showtime.find(filter)
       .populate("movieId")
@@ -111,6 +157,7 @@ const deleteShowtime = asyncHandler(async (req, res) => {
 
 module.exports = {
   getShowtimesByProvince,
+  getShowtimeByCinema,
   getOneShowtimes,
   createNewShowtime,
   deleteShowtime,
--- routes/showtimeRoutes.js
+++ routes/showtimeRoutes.js
@@ -4,9 +4,17 @@ const ShowtimeController = require("../controllers/showtimeController");
 const verifyJWT = require("../middleware/verifyJWT");
 router.route("/").post(ShowtimeController.createNewShowtime);
 
+router;
+router
+  .route("/:id")
+
+  .delete(ShowtimeController.deleteShowtime);
+
+router
+  .route("/cinema/:cinemaId/:date")
+  .get(ShowtimeController.getShowtimeByCinema);
 router
   .route("/:movieId/:provinceId/:date")
-  .get(ShowtimeController.getShowtimesByProvince)
-  .delete(ShowtimeController.deleteShowtime);
+  .get(ShowtimeController.getShowtimesByProvince);
 
 module.exports = router;
